1a5d4ddd4748f7caf74727e73f76e28cfab0a6b3
Txvinxia <ttevin53@gmail.com>
2025-04-09 10:49:25 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Create from "./components/Create";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <NavBar
+        drawerWidth={200}
+        content={
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        }
+      />
     </div>
   );
 }
